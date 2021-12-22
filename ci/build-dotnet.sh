b2122f7c47e8e369923a4d05db761cd77d088b16
Jason <jason.bolonski@gmail.com>
2021-12-22 18:16:48 -0500

    working pipeline

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -4,13 +4,3 @@ cd game-monitor-repo
 dotnet restore
 dotnet build
 dotnet publish
-
-# cd game-monitor-repo/MiniProcess
-# dotnet restore
-# dotnet build
-# dotnet publish
-
-# cd ../game-monitor-repo/GameMonitor
-# dotnet restore
-# dotnet build
-# dotnet publish
