3d2f71d4487924f68057fe6f6297f3931d4297b1
Jason <jason.bolonski@gmail.com>
2021-12-23 13:16:54 -0500

    .

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+mkdir Output
+
 cd game-monitor-repo
 dotnet restore
-dotnet build /nologo /p:PublishProfile=Release /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:configuration="Release" /p:DesktopBuildPackageLocation="GameMonitor.zip"
-#dotnet publish
+#dotnet build
+dotnet publish
 
-#mkdir Output
-#cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output
-#cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output
-#cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output
-
-#dotnet build WebApplicationDeploy.sln /nologo /p:PublishProfile=Release /p:PackageLocation="C:SomePathpackage" /p:OutDir="C:SomePathout" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /maxcpucount:1 /p:platform="Any CPU" /p:configuration="Release" /p:DesktopBuildPackageLocation="C:SomePathpackagepackage.zip"
+mkdir Output
+cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output
