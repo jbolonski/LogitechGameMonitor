79170a4da85d23ccd4334b91a6e92e1366a1094d
Jason <jason.bolonski@gmail.com>
2021-12-23 11:28:25 -0500

    .

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -5,5 +5,11 @@ dotnet restore
 dotnet build
 dotnet publish
 
-ls MiniProcess/bin/Debug/net6.0/win-x64/publish
-ls GameMonitor/bin/Debug/net6.0/win-x64/publish
+#ls MiniProcess/bin/Debug/net6.0/win-x64/publish
+#ls GameMonitor/bin/Debug/net6.0/win-x64/publish
+mkdir Output
+cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output\
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output\
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output\
+
+ls Output
