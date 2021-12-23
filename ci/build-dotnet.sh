70024baeb222ca68c8208409affef6d18fd37ed2
Jason <jason.bolonski@gmail.com>
2021-12-23 11:36:14 -0500

    .

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -8,8 +8,8 @@ dotnet publish
 #ls MiniProcess/bin/Debug/net6.0/win-x64/publish
 #ls GameMonitor/bin/Debug/net6.0/win-x64/publish
 mkdir Output
-cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output\
-cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output\
-cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output\
+cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output
+cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output
 
 ls Output
