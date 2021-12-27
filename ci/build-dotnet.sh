27ff8c89ffb364bc8c0e20a0ae0ebdfee1e8ff75
Jason <jason.bolonski@gmail.com>
2021-12-27 16:30:21 -0500

    test adding zip

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -12,3 +12,6 @@ mkdir Output
 cp MiniProcess/bin/Debug/net6.0/win-x64/publish/MiniProcess.exe Output
 cp GameMonitor/bin/Debug/net6.0/win-x64/publish/GameMonitor.exe Output
 cp GameMonitor/bin/Debug/net6.0/win-x64/publish/config.txt Output
+
+cd Output
+zip GameMonitor.zip MiniProcess.exe GameMonitor.exe config.txt
