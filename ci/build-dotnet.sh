f409e482dbf74465f96e3a800379f98cc5431900
Jason <jason.bolonski@gmail.com>
2022-01-03 16:18:38 -0500

    .

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#apt update -y -q
-#apt install zip -y -q
+apt update -qq -y
+apt install -o:Dpkg::Use-Pty=0 -qq zip -y
 
 cd game-monitor-repo
 dotnet restore
@@ -29,6 +29,6 @@ echo >Output/uninstall.ps1 'sc.exe delete "Game Monitor Service"'
 # Create Zip File
 #####################################################
 cd Output
-#zip GameMonitor.zip MiniProcess.exe GameMonitorService.exe config.txt install.ps1 uninstall.ps1
+zip GameMonitor.zip MiniProcess.exe GameMonitorService.exe config.txt install.ps1 uninstall.ps1
 
 ls
