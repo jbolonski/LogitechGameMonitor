dbefd63975556830bde12a44c3ff70d1616cb716
Jason <jason.bolonski@gmail.com>
2022-01-06 01:40:58 -0500

    fix linux build

--- ci/build-dotnet.sh
+++ ci/build-dotnet.sh
@@ -16,8 +16,8 @@ mkdir Output
 # Copy Files to Output Directory
 #####################################################
 cp MiniProcess/bin/Release/net6.0/win-x64/publish/MiniProcess.exe Output
-cp GameMonitorService/bin/Release/net6.0/win-x64/publish/GameMonitorService.exe Output
-cp GameMonitorService/bin/Release/net6.0/win-x64/publish/config.txt Output
+cp GameMonitorService/bin/Release/net6.0-windows/win-x64/publish/GameMonitorService.exe Output
+cp GameMonitorService/bin/Release/net6.0-windows/win-x64/publish/config.txt Output
 
 #####################################################
 # Create Install/Uninstall Scripts for the Service
